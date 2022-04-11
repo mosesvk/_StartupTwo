9a41f4de8a46ad0bf08f9a81b5decf193885b262
mosesvk <kaumatule93@gmail.com>
2022-04-10 22:43:53 -0600

    working on navbar

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
+import Image from 'next/image'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
 
 import './navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between items-center	py-8 px-24'>
+      <div className='flex-1 flex justify-start items-start'>
+        <div className='mr-8'>
+          <Image src={logo} alt='logoImg' height='62.56px' width='60.02px'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
